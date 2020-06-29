9eded1faea9e75196731ebc84cf6588e0c709a89
wangjie <missgud@163.com>
2020-06-29 15:13:31 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -2,6 +2,9 @@ package com.sunnyweather.android
 
 import android.app.Application
 import android.content.Context
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 
 class SunnyWeatherApplication : Application() {
 
@@ -18,5 +21,17 @@ class SunnyWeatherApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
+
+        val strategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(false)  // 是否显示线程信息，默认为ture
+            .methodCount(1)         // 显示的方法行数
+            .methodOffset(0)        // 隐藏内部方法调用到偏移量
+            .tag("tag")
+            .build()
+        Logger.addLogAdapter(object : AndroidLogAdapter(strategy) {
+            override fun isLoggable(priority: Int, tag: String?): Boolean {
+                return BuildConfig.DEBUG
+            }
+        })
     }
 }
--- app/src/main/java/com/sunnyweather/android/logic/network/ServiceCreator.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/ServiceCreator.kt
@@ -1,5 +1,7 @@
 package com.sunnyweather.android.logic.network
 
+import com.sunnyweather.android.LoggerInterceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,6 +10,7 @@ object ServiceCreator {
     private const val BASE_URL = "https://api.caiyunapp.com"
 
     private val retrofit = Retrofit.Builder()
+        .client(OkHttpClient().newBuilder().addInterceptor(LoggerInterceptor()).build())
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
